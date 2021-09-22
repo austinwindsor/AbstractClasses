163b4a1d1802d5e862616b9798a9098829da42ab
Austin Windsor <windsor929@gmail.com>
2021-09-22 12:57:09 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(name='abstract_classes',
+      version='1.0',
+      description='Custom Classes',
+      author='Austin Windsor',
+      # url='https://www.python.org/sigs/distutils-sig/',
+      packages=setuptools.find_packages(),
+     )
